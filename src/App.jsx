c031eaa65fad7c048e74e149b70abc0c8bcb4b81
Your Name01~git <saifsahai2104@gmail.com>
2024-03-16 03:40:02 +0600

    I have sat up environment before doing Assignment-7

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='max-w-[1200px] mx-auto text-center'>
+      <h1 className='text-3xl'>Saif Sultan Meizan</h1>
+    </div>
+  )
+}
+
+export default App
