cdb01572b20ce666cc3f8576a98cbd55b12fddbd
Your Name01~git <saifsahai2104@gmail.com>
2024-03-17 00:28:53 +0600

    Our recipe section has done

--- src/Recipes.jsx
+++ src/Recipes.jsx
@@ -1,26 +1,80 @@
 const Recipes = () => {
     return (
         <div className="mt-12">
-            <div className="space-y-5">
-                <h1 className="text-4xl font-bold">Our Recipes</h1>
-                <p className="text-base font-normal">Our Recipes offers a diverse culinary repertoire to inspire home cooks with creative and delicious dishes.</p>
+            <div className="space-y-6">
+                <h1 className="font-semibold text-4xl">Our Recipes</h1>
+                <p className="">Our Recipes offers a diverse culinary repertoire to inspire home cooks with creative and delicious dishes.</p>
             </div>
-            <div className="mt-11">
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <div className="w-[331px] h-[200px] style={{backgrounImage: 'url(https://images.unsplash.com/photo-1555126634-323283e090fa?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'}}">
-
-                    </div>
+            <div className="flex gap-6 justify-center items-center">
+            <div className="mt-11 flex gap-6">
+                <div className="card w-80 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src="https://i.ibb.co/n3SGTNs/photo-1602253057119-44d745d9b860.jpg" alt="Shoes" className="rounded-xl" />
+                    </figure>
                     <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
+                        <h2 className="card-title">Spaghetti Bolognese</h2>
+                        <p>Classic Italian pasta dish with savory meat sauce.</p>
+                        <div className="card-body space-y-4">
+                            <div className="space-y-4">
+                                <p className="font-medium text-lg">Ingredients: 6</p>
+                                <ul>
+                                    <li>500g ground beef</li>
+                                    <li>1 onion, chopped</li>
+                                    <li>2 cloves garlic, minced</li>
+                                </ul>
+                            </div>
+                            <div className="flex  justify-between">
+                                <div className="flex justify-center gap-2">
+                                    <img className="w-6 h-6" src="https://img.icons8.com/?size=24&id=86136&format=png" alt="" />
+                                    <p><span>30</span> minutes</p>
+                                </div>
+                                <div className="flex justify-center gap-2">
+                                    <img className="w-6 h-6" src="https://img.icons8.com/?size=64&id=tUQmHTb6ANnA&format=png" alt="" />
+                                    <p><span>600</span> calories</p>
+                                </div>
+                            </div>
+                            <div className="card-actions justify-start">
+                                <button className="btn bg-[#0BE58A] rounded-full">Want to Cook</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div>
-
+                <div className="card w-80 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src="https://i.ibb.co/n3SGTNs/photo-1602253057119-44d745d9b860.jpg" alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Spaghetti Bolognese</h2>
+                        <p>Classic Italian pasta dish with savory meat sauce.</p>
+                        <div className="card-body space-y-4">
+                            <div className="space-y-4">
+                                <p className="font-medium text-lg">Ingredients: 6</p>
+                                <ul>
+                                    <li>500g ground beef</li>
+                                    <li>1 onion, chopped</li>
+                                    <li>2 cloves garlic, minced</li>
+                                </ul>
+                            </div>
+                            <div className="flex  justify-between">
+                                <div className="flex justify-center gap-2">
+                                    <img className="w-6 h-6" src="https://img.icons8.com/?size=24&id=86136&format=png" alt="" />
+                                    <p><span>30</span> minutes</p>
+                                </div>
+                                <div className="flex justify-center gap-2">
+                                    <img className="w-6 h-6" src="https://img.icons8.com/?size=64&id=tUQmHTb6ANnA&format=png" alt="" />
+                                    <p><span>600</span> calories</p>
+                                </div>
+                            </div>
+                            <div className="card-actions justify-start">
+                                <button className="btn bg-[#0BE58A] rounded-full">Want to Cook</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div className="w-[514px] h-[685px] bg-slate-200 border">
+
+            </div>
             </div>
         </div>
     )
